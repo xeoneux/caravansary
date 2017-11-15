05c920bcb2ccfe242e94d64f325edfe34d845a6d
Aayush Kapoor <xeoneux@icloud.com>
2017-11-15 21:15:45 +0530

    Add Reactive Form

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  roomSearch: FormGroup;
+  public submitted: boolean;
+
+  ngOnInit() {
+    this.roomSearch = new FormGroup({
+      checkin: new FormControl(""),
+      checkout: new FormControl("")
+    });
+  }
+
+  onSubmit({ value, valid }: { value: RoomSearch; valid: boolean }) {
+    console.log(value);
+  }
+}
+
+export interface RoomSearch {
+  checkin: string;
+  checkout: string;
 }
--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
