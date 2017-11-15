2fa88350fd6df777b4d7abf722f9799985b092ea
Aayush Kapoor <xeoneux@icloud.com>
2017-11-15 15:24:49 +0530

    Add Reservation Response

--- src/main/java/com/xeoneux/caravansary/rest/ReservationResource.java
+++ src/main/java/com/xeoneux/caravansary/rest/ReservationResource.java
@@ -1,14 +1,12 @@
 package com.xeoneux.caravansary.rest;
 
+import com.xeoneux.caravansary.request.ReservationRequest;
 import com.xeoneux.caravansary.response.ReservationResponse;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -16,14 +14,27 @@ import java.time.LocalDate;
 @RequestMapping(ResourceConstants.ROOM_RESERVATION_V1)
 public class ReservationResource {
 
-    @RequestMapping(path = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(
+        path = "",
+        method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
     public ResponseEntity<ReservationResponse> getAvailableRooms(
-            @RequestParam(value = "checkin")
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @RequestParam(value = "checkin") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
                     LocalDate checkin,
-            @RequestParam(value = "checkout")
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @RequestParam(value = "checkout") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
                     LocalDate checkout) {
         return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+    }
+
+    @RequestMapping(
+        path = "",
+        method = RequestMethod.POST,
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+        consumes = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
+    public ResponseEntity<ReservationResponse> createReservation(
+            @RequestBody ReservationRequest reservationRequest) {
+        return new ResponseEntity<>(new ReservationResponse(), HttpStatus.CREATED);
     }
 }
