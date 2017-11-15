7367ea0d78acd96bd917fe54516285053c1370d3
Aayush Kapoor <xeoneux@icloud.com>
2017-11-15 14:18:25 +0530

    Add Self and Links Model

---
+++ src/main/java/com/xeoneux/caravansary/model/Links.java
@@ -0,0 +1,14 @@
+package com.xeoneux.caravansary.model;
+
+public class Links {
+
+    private Self self;
+
+    public Self getSelf() {
+        return self;
+    }
+
+    public void setSelf(Self self) {
+        this.self = self;
+    }
+}
---
+++ src/main/java/com/xeoneux/caravansary/model/Self.java
@@ -0,0 +1,14 @@
+package com.xeoneux.caravansary.model;
+
+public class Self {
+
+    private String ref;
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+}
