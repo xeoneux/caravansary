07063e18e70d8268aea9c8a1fb5a3fa91b4298fa
Aayush Kapoor <xeoneux@icloud.com>
2017-11-15 12:33:17 +0530

    Initialise Spring Boot Project

---
+++ src/main/java/com/xeoneux/caravansary/caravansary/CaravansaryApplication.java
@@ -0,0 +1,12 @@
+package com.xeoneux.caravansary.caravansary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaravansaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaravansaryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xeoneux/caravansary/caravansary/CaravansaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xeoneux.caravansary.caravansary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CaravansaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
