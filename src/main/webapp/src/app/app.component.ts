bbe6d7304bbe147efe979c3884f89854e7ccecb6
Aayush Kapoor <xeoneux@icloud.com>
2017-11-16 12:19:45 +0530

    Add Table of Available Rooms

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -7,10 +7,12 @@ import { FormControl, FormGroup } from "@angular/forms";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
+  rooms: Room[];
   roomSearch: FormGroup;
   public submitted: boolean;
 
   ngOnInit() {
+    this.rooms = ROOMS;
     this.roomSearch = new FormGroup({
       checkin: new FormControl(""),
       checkout: new FormControl("")
@@ -20,6 +22,38 @@ export class AppComponent implements OnInit {
   onSubmit({ value, valid }: { value: RoomSearch; valid: boolean }) {
     console.log(value);
   }
+
+  reserveRoom(value: string) {
+    console.log("Room id for reservation: " + value);
+  }
+}
+
+const ROOMS: Room[] = [
+  {
+    id: "2459348",
+    roomNumber: "406",
+    price: "20",
+    links: ""
+  },
+  {
+    id: "2453523",
+    roomNumber: "407",
+    price: "25",
+    links: ""
+  },
+  {
+    id: "2159345",
+    roomNumber: "408",
+    price: "28",
+    links: ""
+  }
+];
+
+export interface Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+  links: string;
 }
 
 export interface RoomSearch {
