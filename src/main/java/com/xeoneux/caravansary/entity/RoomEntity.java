7e25abc57c4a405a52d312cf83703928ed56fa9d
Aayush Kapoor <xeoneux@icloud.com>
2017-11-16 12:53:29 +0530

    Add Room Entity

---
+++ src/main/java/com/xeoneux/caravansary/entity/RoomEntity.java
@@ -0,0 +1,43 @@
+package com.xeoneux.caravansary.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Room")
+public class RoomEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull private Integer roomNumber;
+
+    @NotNull private String price;
+
+    public RoomEntity() {
+        super();
+    }
+
+    public RoomEntity(Integer roomNumber, String price) {
+        super();
+        this.roomNumber = roomNumber;
+        this.price = price;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
